1527a8cef7e15c6fec02a027cc20032ea151a399
raimundoneto01 <raimundogomes826@gmail.com>
2024-09-09 11:15:26 -0300

    criando o modal da Aplicação

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import { HeaderContainer, HeaderContent, NewTransactionsButton } from "./styles";
+import * as Dialog from '@radix-ui/react-dialog';
 import logoimg from '../../assets/logo.svg'
 
 export function Header(){
@@ -7,7 +8,18 @@ export function Header(){
       <HeaderContent>
         <img src={logoimg} alt="" />
 
-        <NewTransactionsButton>Nova Transação</NewTransactionsButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionsButton>Nova Transação</NewTransactionsButton>
+          </Dialog.Trigger>
+          <Dialog.Portal>
+            <Dialog.Overlay/>
+            <Dialog.Content>
+              <Dialog.Title>Nova Transação</Dialog.Title>
+              <Dialog.Close>Fechar Transação</Dialog.Close>
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   )
