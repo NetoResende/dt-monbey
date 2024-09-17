95bedad812b67c7cb25f2375c3240395f1c38eea
raimundoneto01 <raimundogomes826@gmail.com>
2024-09-17 17:18:22 -0300

    Aplicando react hook form / controlled components

--- src/components/NewTransactionsModal/index.tsx
+++ src/components/NewTransactionsModal/index.tsx
@@ -1,37 +1,98 @@
-import * as Dialog from '@radix-ui/react-dialog';
-import { DailogCloseButton, DaylogContent, DaylogOverlay, TransactionsType, TransactionsTypeButton} from './styles';
-import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react';
+import * as Dialog from "@radix-ui/react-dialog";
+import {
+  DailogCloseButton,
+  DaylogContent,
+  DaylogOverlay,
+  TransactionsType,
+  TransactionsTypeButton,
+} from "./styles";
+import { ArrowCircleDown, ArrowCircleUp, X } from "phosphor-react";
+import { Controller, useForm } from "react-hook-form";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const newTransactonFormSchema = z.object({
+  description: z.string(),
+  price: z.number(),
+  category: z.string(),
+  type: z.enum(["income", "outcome"]),
+});
+
+type NewTransactionFormInput = z.infer<typeof newTransactonFormSchema>;
 
 export function NewTransactionsModal() {
+  const {
+    control,
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<NewTransactionFormInput>({
+    resolver: zodResolver(newTransactonFormSchema),
+    defaultValues: {
+      type: 'income'
+    }
+  });
+
+  async function handlerSearchForm(data: NewTransactionFormInput) {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+
+    console.log(data);
+  }
+
   return (
     <Dialog.Portal>
       <DaylogOverlay />
 
       <DaylogContent>
         <Dialog.Title>Nova Transação</Dialog.Title>
-          <DailogCloseButton>
-            <X size={24}/>
-          </DailogCloseButton>
-          <form >
-            <input type="text" placeholder='Descrição' required/>
-            <input type="number" placeholder='Preço' required/>
-            <input type="text" placeholder='Categoria' required/>
+        <DailogCloseButton>
+          <X size={24} />
+        </DailogCloseButton>
+        <form onSubmit={handleSubmit(handlerSearchForm)}>
+          <input
+            type="number"
+            placeholder="Preço"
+            required
+            {...register("price", { valueAsNumber: true })}
+          />
+          <input
+            type="text"
+            placeholder="Categoria"
+            required
+            {...register("category")}
+          />
+          <input
+            type="text"
+            placeholder="Descrição"
+            required
+            {...register("description")}
+          />
 
-            <TransactionsType>
-              <TransactionsTypeButton variant='income' value='income'>
-                <ArrowCircleDown size={24}/>
-                Entrada
-              </TransactionsTypeButton>
-              <TransactionsTypeButton variant='outcome' value='outcome'>
-                <ArrowCircleUp size={24}/>
-                Saída
-              </TransactionsTypeButton>
-            </TransactionsType>
+          <Controller
+            control={control}
+            name="type"
+            render={({field}) => {
+              console.log(field);
+              
+              return (
+                <TransactionsType onValueChange={field.onChange} value={field.value}>
+                  <TransactionsTypeButton variant="income" value="income">
+                    <ArrowCircleDown size={24} />
+                    Entrada
+                  </TransactionsTypeButton>
+                  <TransactionsTypeButton variant="outcome" value="outcome">
+                    <ArrowCircleUp size={24} />
+                    Saída
+                  </TransactionsTypeButton>
+                </TransactionsType>
+              );
+            }}
+          />
 
-            <button type='submit'>
-              Cadastrar
-            </button>
-          </form>
+          <button type="submit" disabled={isSubmitting}>
+            Cadastrar
+          </button>
+        </form>
       </DaylogContent>
     </Dialog.Portal>
   );
--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,13 +1,36 @@
 import { MagnifyingGlass } from "phosphor-react";
 import { SearchFormContainer } from "./styles";
+import { useForm } from "react-hook-form";
+import * as zod from 'zod';
+import { zodResolver} from '@hookform/resolvers/zod'
 
+
+const searchFormSchema = zod.object({
+  query: zod.string(),
+})
+type SearchFormInput = zod.infer<typeof searchFormSchema>;
 
 export function SearchForm(){
-  return (
-   <SearchFormContainer>
-      <input type="text" placeholder="Busque por transações" />
+  const { register, handleSubmit, formState: {isSubmitting} } = useForm <SearchFormInput>({
+    resolver: zodResolver(searchFormSchema)
+  })
 
-      <button>
+  async function handlerSearchTransactions(data: SearchFormInput){
+    await new Promise(resolve => setTimeout(resolve, 2000))
+
+   console.log(data);
+  }
+
+
+  return (
+   <SearchFormContainer onSubmit={handleSubmit(handlerSearchTransactions)}>
+      <input 
+        type="text" 
+        placeholder="Busque por transações"
+        {...register('query')}
+         />
+
+      <button disabled={isSubmitting}>
         <MagnifyingGlass/>
          Buscar
       </button>
